83bb4c86c4dc6050aaae4a6d436c8d7cce835c7b
pc20211045 <115679875+pc20211045@users.noreply.github.com>
2022-11-17 11:17:22 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
 let x = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
+    x = input.lightLevel()
+    basic.showNumber(x)
+    led.plotBarGraph(
+    x,
+    255
+    )
 })
 input.onButtonPressed(Button.B, function () {
-    x = input.lightLevel()
-    if (x < 50) {
-        led.setBrightness(255)
-    } else if (x > 50 && x < 100) {
-        led.setBrightness(128)
-    } else if (x > 100 && x < 200) {
-        led.setBrightness(64)
-    } else if (x > 200) {
-        led.setBrightness(0)
-    }
-    basic.showNumber(x)
+    led.setBrightness(255 - input.lightLevel())
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+	
 })
